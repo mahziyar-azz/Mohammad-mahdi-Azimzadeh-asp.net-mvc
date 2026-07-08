74572a42d80477c2eb440bbd1937936e50e26eba
mahziyar-azz <mahziyar.azimzadeh@gmail.com>
2026-07-08 18:27:20 +0330

    fix thr login stuff

--- Azimzadeh MVC project/Areas/Admin/AdminAreaRegistration.cs
+++ Azimzadeh MVC project/Areas/Admin/AdminAreaRegistration.cs
@@ -15,6 +15,13 @@ namespace Azimzadeh_MVC_project.Areas.Admin
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
+                "Admin_Login",
+                "Admin/Login",
+                new { controller = "Dashboard", action = "Login" },
+                namespaces: new[] { "Azimzadeh_MVC_project.Areas.Admin.Controllers" }
+            );
+
+            context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
                 new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional },
